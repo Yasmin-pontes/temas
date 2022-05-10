64ceca039771c3267a6c396e8b136bd3f40b86f3
unknown <yasmin10032005@gmail.com>
2022-05-09 21:19:29 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<div class="row">
+		<div class="col-md-12 rodape">
+			<p class="rodape1">autoria: @yasmin pontes </p>            
+			<P class="rodape1"> contato: articunonoticias@gmail.com </p>
+
+		</div>
+	</div>
+	<?php wp_footer();  ?>
+</body>
+</html>
